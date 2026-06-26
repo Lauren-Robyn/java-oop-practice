491abe3a8bec73bd52cd0b143fbff3c1d145c5c8
Lauren <lauren.r.s.steenkamp@gmail.com>
2026-06-26 11:31:10 +0200

    Exercise 2: Encapsulation, data validation, defensive setters

--- encapsulation-excercise-1/src/main/java/com/practice/Person.java
+++ encapsulation-excercise-1/src/main/java/com/practice/Person.java
@@ -3,9 +3,13 @@ package com.practice;
 public class Person {
 
         // TODO: add private field
-
+        private String name;
         // TODO: implement setter
 
-        // TODO: implement getter
+        public void setName(String name) {
+        this.name = name;}
 
+        // TODO: implement getter
+        public String getName() {
+        return name;}
 }
